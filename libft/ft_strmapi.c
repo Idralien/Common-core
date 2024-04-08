44ab2cd5b8b314c33429b736f7f207fb7027b88b
Idralien <145843404+Idralien@users.noreply.github.com>
2024-04-08 13:38:27 +0200

    Add files via upload

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brsantsc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/02 15:23:16 by brsantsc          #+#    #+#             */
+/*   Updated: 2024/04/04 11:52:44 by brsantsc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	unsigned int	len;
+	char			*result;
+
+	if (!s)
+		return (NULL);
+	len = ft_strlen(s);
+	result = malloc(len * sizeof(char) + 1);
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		result[i] = (*f)(i, s[i]);
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
