08f4441e26edb51f8df3c95b879ae49ba2d6605f
Idralien <145843404+Idralien@users.noreply.github.com>
2024-04-24 14:02:28 +0200

    Add files via upload

---
+++ pintf_test/print_hex.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf_hex.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brsantsc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/24 13:24:23 by brsantsc          #+#    #+#             */
+/*   Updated: 2024/04/24 13:24:34 by brsantsc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static char	*create_string(unsigned int value, int *strlen)
+{
+	int				i;
+	unsigned int	temp;
+	char			*str;
+
+	i = 0;
+	temp = value;
+	while (temp != 0)
+	{
+		temp = temp / 16;
+		i++;
+	}
+	str = calloc(i + 1, sizeof(char));
+	*strlen = i - 1;
+	return (str);
+}
+
+int	print_hex(unsigned int value, int asc)
+{
+	unsigned int	tempval;
+	char			*printout;
+	int				i;
+	int				*iptr;
+
+	iptr = &i;
+	tempval = value;
+	printout = create_string(value, iptr);
+	if (!printout)
+		return (0);
+	while (tempval != 0)
+	{
+		if ((tempval % 16) < 10)
+			printout[i] = (tempval % 16) + 48;
+		else
+			printout[i] = (tempval % 16) + asc;
+		tempval = tempval / 16;
+		i--;
+	}
+	ft_putstr_fd(printout, 1);
+	i = ft_strlen(printout);
+	free(printout);
+	if (value == 0)
+		i += print_char('0');
+	return (i);
+}
---
+++ pintf_test/print_unsigned.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_unsigned.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brsantsc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/24 13:26:34 by brsantsc          #+#    #+#             */
+/*   Updated: 2024/04/24 13:27:12 by brsantsc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static void	printout(unsigned int nb)
+{
+	if (nb > 9)
+		print_unsigned(nb / 10);
+	if (nb <= 9)
+	{
+		ft_putchar_fd(nb + 48, 1);
+		return ;
+	}
+	ft_putchar_fd((nb % 10) + 48, 1);
+}
+
+int	print_unsigned(unsigned int nb)
+{
+	unsigned int	i;
+
+	printout(nb);
+	i = 1;
+	while (nb > 9)
+	{
+		nb = nb / 10;
+		i++;
+	}
+	return (i);
+}
