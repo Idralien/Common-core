1bd7a82922afd2850985b797d82530eab642fd50
Brent Santschi <145843404+Idralien@users.noreply.github.com>
2025-08-15 18:00:44 +0200

    Full Libft usable library

---
+++ libft (usable library)/src/memory/ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brsantsc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/04 11:46:38 by brsantsc          #+#    #+#             */
+/*   Updated: 2024/04/04 11:46:50 by brsantsc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/libft.h"
+
+void	*ft_calloc(size_t counter, size_t size)
+{
+	void	*ptr;
+
+	ptr = (void *)malloc(counter * size);
+	if (!ptr)
+		return (NULL);
+	ft_memset(ptr, 0, counter * size);
+	return (ptr);
+}
